7552c90bcfcc3550772b950ff1cf4e12be41d9ca
Frank TR <13178936+d-fect@users.noreply.github.com>
2019-07-20 01:00:24 +0200

    Hello Godot
    Initial commit of Hello Godot-exercise in Section 1.

---
+++ HelloGodot/HelloWorld.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _ready():
+	var greeting = "Hello"
+	var number = 2
+	print(greeting + " World! You're #" + str(number) + "!")
+	
+	# Exercise section 1.4 
+	var wait = "What are we waiting for?"
+	var thing = "GODOT"
+	print (wait + " " + thing + "!")
