090a3fd3d994a0f6385161a2329360696ebda3f6
Frank TR <13178936+d-fect@users.noreply.github.com>
2019-07-20 16:26:53 +0200

    Added a signal and created a new func
    Added a signal from PlayerText to LoonyLips.
    
    Added a function to update DisplayText.

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -3,8 +3,16 @@ extends Control
 func _ready():
 	var songPrompts = ["OGG", "MC Solaar", "Nouveau Western", "liked it"]
 	var songStory = "%s listened to the %s song %s, and he %s!"
-	print(songStory % songPrompts)
+#	print(songStory % songPrompts)
 	
-	get_node("HeaderText").text = "Welcome!"
-	# $DisplayText.text = "This is some text!"		# $NodeName is the same as using get_node("NodeName)
-	$DisplayText.text = (songStory % songPrompts)
+	get_node("HeaderText").text = "Your story!"
+#	$DisplayText.text = "This is some text!"		# $NodeName is the same as using get_node("NodeName)
+	$VBoxContainer/DisplayText.text = (songStory % songPrompts)
+	
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
