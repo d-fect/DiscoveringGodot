5ee7f3bc872d1d8804f5d2a9d6b085a7acce7a82
Frank TR <13178936+d-fect@users.noreply.github.com>
2019-07-22 20:33:46 +0200

    Addet a dictionary for multiple story templates

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -2,31 +2,43 @@ extends Control
 
 # Variable declarations
 var player_words = []
-var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day!"
-var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+var template = [
+		{
+		"prompts" : ["a name", "a noun", "an adverb", "an adjective"],
+		"story":"Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day!"
+		},
+		{
+		"story" : "Everyone %s %s! She was %s and %s!",
+		"prompts" : ["an emotion", "a name", "an adjective", "another adjective"]
+		}
+		]
+var current_story
 
 onready var PlayerText  = $VBoxContainer/InputContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
-	$VBoxContainer/InputContainer.visible = true
-	$VBoxContainer/RestartContainer.visible = false
 	get_node("HeaderText").text = "Your story!"
 	DisplayText.text = "Welcome to Loony Lips! With your help, we're going to create a great story together. "
-	# TODO: Insert timer here
+	# TODO: Insert timer here. Or not...
+	set_current_story()
 	check_player_words_length()
-	
+
+
+func set_current_story():
+	randomize()
+	current_story = template[randi() % template.size()]
+
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
 
 
 func _on_TextureButton_pressed():
-	add_to_player_words()
-
-
-func _on_RestartButton_pressed():
-	_ready()
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
 
 func add_to_player_words():
@@ -37,26 +49,28 @@ func add_to_player_words():
 
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 
 
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_game()
 	else:
 		prompt_player()
 
 
 func tell_story():
-	$VBoxContainer/InputContainer.visible = false
-	$VBoxContainer/RestartContainer.visible = true
-	DisplayText.text = story % player_words
-	player_words.clear()
+	DisplayText.text = current_story.story % player_words
 
 
 func prompt_player():
-	DisplayText.text += "\nMay I have " + prompts[player_words.size()] + " please?"
+	PlayerText.grab_focus()
+	DisplayText.text += "\nMay I have " + current_story.prompts[player_words.size()] + " please?"
 
 
-
+func end_game():
+	PlayerText.queue_free()
+	$VBoxContainer/InputContainer/Label.text = "Play again?"
+	$VBoxContainer/InputContainer/TextureButton.grab_focus()
+	tell_story()
 
